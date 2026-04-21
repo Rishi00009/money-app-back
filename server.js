67dc274323facf8d6c8f0061de474fbaa5fe894b
Rishi00009 <your-github-rishirishim0@gmail.com>
2026-04-21 17:09:09 +0530

    Backend: Express server with JWT and mongodb integration

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173', // Your Vite frontend URL
+  origin: 'https://rishi-money-management-app.netlify.app/', // Your Vite frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'x-auth-token']
 }));
